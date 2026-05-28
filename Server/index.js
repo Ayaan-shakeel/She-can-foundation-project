bb6c505de697ccbf87086ec4b5ad5c83ddcb00d9
Hayyaanshakeel <ayyanshakeel899@gmail.com>
2026-05-28 19:02:30 -0400

    fixing our cors frontend url

--- Server/index.js
+++ Server/index.js
@@ -8,8 +8,9 @@ dotenv.config();
 const app=express();
 app.use(express.json());
 app.use(cors({
-    origin:"http://localhost:5174"
-}));
+    origin:["http://localhost:5174",
+        "https://she-can-foundation-project-two.vercel.app"]
+    }));
 app.use("/api/form",formRouter)
 connectDB();
 app.listen(process.env.PORT || 7000,()=>{
